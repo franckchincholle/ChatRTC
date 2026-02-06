4c127972311d9c8cd98b47cc483765d0392b598a
franckchincholle <franck.chincholle@epitech.eu>
2026-02-06 10:30:52 +0100

    Starting server.repository file

--- backend/src/repositories/server.repository.ts
+++ backend/src/repositories/server.repository.ts
@@ -1,7 +1,26 @@
-// Repository pour Server
-// Accès base de données
+import { Server, ServerMember, CreateServerDTO } from '../types/server.types';
 
 export class ServerRepository {
+
+  async create(data: CreateServerDTO & { ownerId: string}): Promise<Server> {
+    const newServer = await this.db.server.save({
+      name: data.name,
+      ownerId: data.ownerId,
+      // inviteCode: this.generateInviteCode(),
+    })
+
+    // await this.addMember(newServer.id, data.ownerId, 'owner');
+
+    // async findById(id: string): Promise<Server | null> {
+    //   // Implement logic find server by id
+    //   throw new Error('Method not implemented.');
+    // }
+
+    return newServer;
+  }
+
+
+
   // TODO: CRUD operations
   // - create(data)
   // - findById(id)
