25177061428b1254319420ba58c20844f3376a01
franckchincholle <franck.chincholle@epitech.eu>
2026-02-12 14:20:50 +0100

    adding message routes

---
+++ backend/src/routes/message.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { messageController } from '../controllers/message.controller';
+import { authenticate } from '../middlewares/auth.middleware';
+
+const router = Router();
+
+// authentication
+router.use(authenticate);
+
+// message routes
+router.post('/', messageController.sendMessage);
+
+export default router;
