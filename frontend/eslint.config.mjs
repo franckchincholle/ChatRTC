72bfc5427cd8816f1bf51ec7d785d9f92eb2609e
Yannis <yannis.gaspard@epitech.eu>
2026-03-18 14:59:30 +0100

    fixed eslint config to fix nextVitals error

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  ...nextTs,
   globalIgnores([
     ".next/**",
     "out/**",
